0631abaf7d4644bcfc1e1f991fb59bf0aa5f0c6c
VuHDHE186822 <vuhdhe186822@fpt.edu.vn>
2024-10-15 03:10:40 +0700

    add blog GUI

---
+++ src/java/model/Tag.java
@@ -0,0 +1,35 @@
+public class Tag {
+    private int tagId;
+    private String tagName;
+
+    // Constructors
+    public Tag(int tagId, String tagName) {
+        this.tagId = tagId;
+        this.tagName = tagName;
+    }
+
+    // Getters and Setters
+    public int getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(int tagId) {
+        this.tagId = tagId;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "tagId=" + tagId +
+                ", tagName='" + tagName + '\'' +
+                '}';
+    }
+}
