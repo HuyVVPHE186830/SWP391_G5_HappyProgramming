7a8a9c15868c5f4348c1165af5f1d4c35f228ce1
VuHDHE186822 <vuhdhe186822@fpt.edu.vn>
2024-10-24 12:42:18 +0700

    leftadmin GUI

---
+++ src/java/constant/AIConstant.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package constant;
+
+/**
+ *
+ * @author Sapphire
+ */
+public class AIConstant {
+        public static final String OPENAI_API_KEY = "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";
+
+}
