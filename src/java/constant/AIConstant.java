478defcb658f1de8e3464d1e982214f577dd4c2f
VuHDHE186822 <vuhdhe186822@fpt.edu.vn>
2024-10-24 15:31:12 +0700

    Merge origin/master

--- src/java/constant/AIConstant.java
+++ src/java/constant/AIConstant.java
@@ -10,6 +10,6 @@ package constant;
  */
 public class AIConstant {
 
-    public static final String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
+    public static final String OPENAI_API_KEY = "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";
 
 }
