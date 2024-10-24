8469fdde30be659d2a52f62bdf1ef4ce92d53c6b
VuHDHE186822 <vuhdhe186822@fpt.edu.vn>
2024-10-24 12:44:24 +0700

    leftadmin GUI

--- src/java/constant/AIConstant.java
+++ src/java/constant/AIConstant.java
@@ -9,7 +9,6 @@ package constant;
  * @author Sapphire
  */
 public class AIConstant {
-//        public static final String OPENAI_API_KEY = "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";
 public static final String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
 
 }
