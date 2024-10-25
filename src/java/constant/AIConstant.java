c55e6aa22e413ad194b950a4a683d5a9852d5303
KGJG\yeuda <trucnghe186655@fpt.edu.vn>
2024-10-25 15:37:10 +0700

    dailyConstant commit

---
+++ src/java/constant/AIConstant.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package constant;
+
+/**
+ *
+ * @author yeuda
+ */
+public class AIConstant {
+        public static final String OPENAI_API_KEY = "";
+
+}
