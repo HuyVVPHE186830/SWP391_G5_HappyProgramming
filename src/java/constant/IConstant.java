d8a62a86c3b6d8f40d045e4acc41baf3bb238d41
KGJG\yeuda <trucnghe186655@fpt.edu.vn>
2024-10-27 00:52:36 +0700

    aprove commit

--- src/java/constant/IConstant.java
+++ src/java/constant/IConstant.java
@@ -6,11 +6,7 @@ public class IConstant {
 
     public static final String GOOGLE_CLIENT_SECRET = "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";
 
-<<<<<<< HEAD
     public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/HappyProgramming/login";
-=======
-    public static final String GOOGLE_REDIRECT_URI = "http://localhost:9999/HappyProgramming/login";
->>>>>>> ecd77f6f80f08ac9b7b7559681fe26345c65fc2c
 
     public static final String GOOGLE_GRANT_TYPE = "authorization_code";
 
@@ -20,12 +16,7 @@ public class IConstant {
 
     public static final String SESSION_PRODUCT = "listProducts";
 
-<<<<<<< HEAD
     public static final String GOOGLE_REDIRECT_RESET = "http://localhost:8080/HappyProgramming/verify";
 
 }
-=======
-    public static final String GOOGLE_REDIRECT_RESET = "http://localhost:9999/HappyProgramming/verify";
 
-}
->>>>>>> ecd77f6f80f08ac9b7b7559681fe26345c65fc2c
